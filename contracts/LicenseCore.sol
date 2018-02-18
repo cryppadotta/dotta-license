06d80bb724d625971ff8593a5a64777fc90186e8
Dotta <bippadotta@protonmail.com>
2018-02-18 14:21:03 -0600

    added core and test

--- contracts/LicenseCore.sol
+++ contracts/LicenseCore.sol
@@ -2,7 +2,29 @@ pragma solidity ^0.4.18;
 
 import "./LicenseMinting.sol";
 
-contract LicenseCore is LicenseMinting {
-    function LicenseCore() public {
-    }
+contract LicenseCore is LicenseSale {
+  address public newContractAddress;
+
+  function LicenseCore() public {
+    paused = true;
+
+    ceoAddress = msg.sender;
+    cooAddress = msg.sender;
+    cfoAddress = msg.sender;
+    withdrawalAddress = msg.sender;
+  }
+
+  function setNewAddress(address _v2Address) public onlyCEO whenPaused {
+    newContractAddress = _v2Address;
+    ContractUpgrade(_v2Address);
+  }
+
+  function() external payable {
+    assert(false);
+  }
+
+  function unpause() public onlyCEO whenPaused {
+    require(newContractAddress == address(0));
+    super.unpause();
+  }
 }
---
+++ contracts/LicenseCoreTest.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+import "./LicenseCore.sol";
+
+contract LicenseCoreTest is LicenseCore {
+
+  function LicenseTest() public {
+  }
+
+  function fund() public payable returns (bool) {
+    return true;
+  }
+
+  function timeNow public constant returns (uint256) {
+    return now;
+  }
+}
